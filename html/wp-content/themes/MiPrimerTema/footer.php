8fb6ebe916ca2f77baec0f741d8feb4dce3f3bb5
iago <ipampillonvieitez@danielcastelao.org>
2020-11-18 13:10:18 +0100

    actualizacion tema

--- html/wp-content/themes/MiPrimerTema/footer.php
+++ html/wp-content/themes/MiPrimerTema/footer.php
@@ -1,2 +1,17 @@
 <?php
-echo "<h1> FPPTERRRES </h1>";
+echo "<h1> totoro </h1>"; ?>
+<img src="<?php echo get_stylesheet_directory_uri();?>/totoro.jpg">
+
+<h1>hola</h1>
+<p>que tal estas</p>
+<img src="<?php echo get_stylesheet_directory_uri();?>/descarga.jpg">
+
+
+
+
+
+
+
+
+
+
